59883eb4d377c2bee888bfaa2c443ab98f2cf678
saray <saraytapia0@gmail.com>
2024-09-07 20:00:28 -0400

    estructura total a pagar

---
+++ index.js
@@ -0,0 +1,7 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+total_a_pagar = document.querySelector(".valor-total");
+total_a_pagar.innerHTML = parseInt("cantidad.innerHTML") * "precio"
